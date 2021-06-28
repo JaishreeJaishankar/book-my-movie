9c0a4ee482040342ff596a4ecfd5c8cd28d2f643
Jaishree <jaishree.jaishankar@riseup.net>
2021-06-28 23:17:41 +0530

    Code refactoring and formatting Details page

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -1,188 +1,140 @@
 import React, { useEffect, useState } from "react";
-import Header from '../../common/header/Header';
-import Typography from '@material-ui/core/Typography';
-import './Details.css';
+import { Link } from 'react-router-dom';
 import YouTube from 'react-youtube';
+
+import Typography from '@material-ui/core/Typography';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
-import { Link } from 'react-router-dom';
+
+import Header from '../../common/header/Header';
+import './Details.css';
+
 
 const Details = (props) => {
+    
+    const[movieData, setMovieData] = useState({
+        genres: [],
+        trailer_url: "",
+        artists: []
+        });
 
+    useEffect(() => {
+        let movieInfo = null;    
+        fetch(props.baseUrl + "movies/" + props.match.params.id , {
+            method: "GET",
+            headers: {
+            "Content-Type": "application/json",
+            "Cache-Control": "no-cache",
+            },
+            body: movieInfo,
+        })  
+        .then((response) => response.json())
+        .then((response) => {
+            setMovieData(response);
+        });
+        }, []);
 
-  const[movie, setMovie] = useState({
-    genres: [],
-    trailer_url: "",
-    artists: []
-    });
-
-const [starIcons, setStarIcons] = useState([{
-    id: 1,
-    stateId: "star1",
-    color: "black"
-},
-{
-    id: 2,
-    stateId: "star2",
-    color: "black"
-},
-{
-    id: 3,
-    stateId: "star3",
-    color: "black"
-},
-{
-    id: 4,
-    stateId: "star4",
-    color: "black"
-},
-{
-    id: 5,
-    stateId: "star5",
-    color: "black"
-}]);
-
-
-
-useEffect(() => {
-        let dataMovie = null;
-        
-    fetch(props.baseUrl + "movies/" + props.match.params.id , {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          "Cache-Control": "no-cache",
-        },
-        body: dataMovie,
-      })  
-      .then((response) => response.json())
-      .then((response) => {
-        setMovie(response);
-
-
-    });
-}, []);
-
-
-
-
-const artistClickHandler = (url) => {
-    window.location = url;
-}
-
-
-const startClickHandler = (id) => {
-    let starIconList = [];
-    for (let star of starIcons) {
-        let starNode = star;
-        if (star.id <= id) {
-            starNode.color = "yellow"
+    const videoOptions = {
+        height: '300',
+        width: '700',
+        playerVars: {
+            autoplay: 1
         }
-        else {
-            starNode.color = "black";
+    }
 
+    let initStarIcons = []
+    for(let i = 1; i < 6; i++)
+        initStarIcons.push({
+            id: i,
+            color: "black"
+        }) 
+    
+    const [starIcons, setStarIcons] = useState(initStarIcons);
+
+    const startClickHandler = (id) => {
+        let starIconList = [];
+        for (let star of starIcons) {
+            let starNode = star;
+            if (star.id <= id)
+                starNode.color = "yellow"
+            else
+                starNode.color = "black";
+            starIconList.push(starNode);
         }
-        starIconList.push(starNode);
+        setStarIcons(starIconList);
     }
-    setStarIcons( starIconList);
-}
 
-// let movie = movie;
-const opts = {
-    height: '300',
-    width: '700',
-    playerVars: {
-        autoplay: 1
+    const artistClickHandler = (url) => {
+        window.location = url;
     }
-}
 
-return(
+    return(
+        <div className="details">
+            <Header id={props.match.params.id} baseUrl={props.baseUrl} showBookShowButton="true" />
+            <div className="backToHome">
+                <Typography>
+                    <Link to="/">  &lt; Back to Home</Link>
+                </Typography>
+            </div>
+            <div className="flex-containerDetails">
+                <div className="leftPanel centerAlign">
+                    <img src={movieData.poster_url} alt={movieData.title}/>
+                </div>
 
-<div className="details">
-<Header id={props.match.params.id} baseUrl={props.baseUrl} showBookShowButton="true" />
-<div className="back">
-    <Typography>
-        <Link to="/">  &#60; Back to Home</Link>
-    </Typography>
-</div>
-<div className="flex-containerDetails">
-    <div className="leftDetails">
-        <img src={movie.poster_url} alt={movie.title} />
-    </div>
-
-    <div className="middleDetails">
-        <div>
-            <Typography variant="headline" component="h2">{movie.title} </Typography>
-        </div>
-        <br />
-        <div>
-            <Typography>
-            <span className="bold">Genres: </span> {movie.genres.join(",")}
-            
-            </Typography>
-        </div>
-        <div>
-            <Typography><span className="bold">Duration:</span> {movie.duration} </Typography>
-        </div>
-        <div>
-            <Typography><span className="bold">Release Date:</span> {new Date(movie.release_date).toDateString()} </Typography>
-        </div>
-        <div>
-            <Typography><span className="bold"> Rating:</span> {movie.critics_rating}  </Typography>
-        </div>
-        <div className="marginTop-16">
-            <Typography><span className="bold">Plot:</span> <a href={movie.wiki_url}>(Wiki Link)</a> {movie.storyline} </Typography>
-        </div>
-        <div className="trailer-Container">
-            <Typography>
-                <span className="bold">Trailer:</span>
-            </Typography>
-            <YouTube
-                videoId={movie.trailer_url.split("?v=")[1]}
-                opts={opts}
-                onReady={props._onReady}
-            />
-        </div>
-    </div>
-
-    <div className="rightDetails">
-        <Typography>
-            <span className="bold">Rate this movie: </span>
-        </Typography>
-        {starIcons.map(star => (
-            <StarBorderIcon
-                className={star.color}
-                key={"star" + star.id}
-                onClick={() => startClickHandler(star.id)}
-            />
-        ))}
-
-        <div className="bold marginBottom-16 marginTop-16">
-            <Typography>
-                <span className="bold">Artists:</span>
-            </Typography>
-        </div>
-        <div className="paddingRight">
-            <GridList cellHeight={160} cols={2}>
-                {movie.artists != null && movie.artists.map(artist => (
-                    <GridListTile
-                        className="grid-Tile"
-                        onClick={() => artistClickHandler(artist.wiki_url)}
-                        key={artist.id}>
-                        <img src={artist.profile_url} alt={artist.first_name + " " + artist.last_name} />
-                        <GridListTileBar
-                            title={artist.first_name + " " + artist.last_name}
+                <div className="middlePanel">
+                    <Typography variant="headline" component="h2">{movieData.title}</Typography>
+                    <Typography>
+                        <span className="bold">Genres: </span> {movieData.genres.join(",")}
+                    </Typography>
+                    <Typography><span className="bold">Duration:</span> {movieData.duration} </Typography>
+                    <Typography><span className="bold">Release Date:</span> {new Date(movieData.release_date).toDateString()} </Typography>
+                    <Typography><span className="bold"> Rating:</span> {movieData.critics_rating}  </Typography>
+                    <div className="marginTop-16">
+                        <Typography><span className="bold">Plot:</span> <a href={movieData.wiki_url}>(Wiki Link)</a> {movieData.storyline} </Typography>
+                    </div>
+                    <div className="trailer-Container">
+                        <Typography> <span className="bold">Trailer:</span></Typography>
+                        <YouTube
+                            videoId={movieData.trailer_url.split("?v=")[1]}
+                            opts={videoOptions}
+                            onReady={props._onReady}
                         />
-                    </GridListTile>
-                ))}
-            </GridList>
-        </div>
-    </div>
-</div>
-</div>)
+                    </div>
+                </div>
 
+                <div className="rightPanel">
+                    <Typography><span className="bold">Rate this movie: </span></Typography>
+                    {starIcons.map(star => (
+                        <StarBorderIcon
+                            className={star.color}
+                            key={"star" + star.id}
+                            onClick={() => startClickHandler(star.id)}
+                        />
+                    ))}
+                    <div className="bold marginBottom-16 marginTop-16">
+                        <Typography><span className="bold">Artists:</span></Typography>
+                    </div>
+                    <div className="paddingRight">
+                        <GridList cellHeight={160} cols={2}>
+                            {movieData.artists != null && movieData.artists.map(artist => (
+                                <GridListTile
+                                    className="grid-Tile"
+                                    onClick={() => artistClickHandler(artist.wiki_url)}
+                                    key={artist.id}>
+                                    <img src={artist.profile_url} alt={artist.first_name + " " + artist.last_name} />
+                                    <GridListTileBar
+                                        title={artist.first_name + " " + artist.last_name}
+                                    />
+                                </GridListTile>
+                            ))}
+                        </GridList>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Details;
